7d0c1296cd4b02c89f93e09cad61240c19cca7aa
soledadrv <soledadrv@outlook.com>
2021-08-03 18:13:23 -0300

    creo el server

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const port = 3040;
+const path = require('path');
+
+
+
+
+
+app.listen(port, () => console.log('Servidor corriendo en el puerto ' + port));
