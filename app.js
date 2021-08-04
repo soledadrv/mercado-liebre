eae6e2eabd3ca27daa465c05852f7367d9aab4a9
soledadrv <soledadrv@outlook.com>
2021-08-04 10:33:16 -0300

    agrego los productos con su descripción

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 const port = 3040;
 const path = require('path');
+const { allowedNodeEnvironmentFlags } = require('process');
 
+app.use(express.static('public'));
 
-
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views', 'home.html')));
 
 
 app.listen(port, () => console.log('Servidor corriendo en el puerto ' + port));
