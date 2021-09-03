db326d87d0edd1839e14f15bed93d34f014cc3cd
soledadrv <soledadrv@outlook.com>
2021-09-03 18:35:26 -0300

    termino páginas de login y register

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const path = require('path');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views', 'index.html')));
+app.get('/login', (req, res) => res.sendFile(path.join(__dirname, 'views', 'login.html')));
+app.get('/register', (req, res) => res.sendFile(path.join(__dirname, 'views', 'register.html')));
 
 
 app.listen(port, () => console.log('Servidor corriendo en el puerto ' + port));
